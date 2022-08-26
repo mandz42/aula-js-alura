4caf74bf244542ca0b82aee8ef7542d0b476bffe
mandz <amandna42@gmail.com>
2022-08-26 16:35:16 -0300

    novos exemplos e tópicos adicionados

---
+++ comparacoes.js
@@ -0,0 +1,16 @@
+// comparações
+// == (comparação implícita) - comapara só o valor
+
+const numero = 5;
+const texto = "5";
+
+console.log(numero == texto)
+//true
+
+// === (comparação explícita) - compara o valor e o tipo de variável
+console.log(numero === texto)
+//false
+
+//typeof
+console.log(typeof numero) //number
+console.log(typeof texto) //string
---
+++ template-string.js
@@ -0,0 +1,11 @@
+const nome = "Mandz";
+const idade = 2022-1990;
+const cidadeDeNascimento = "Santo André";
+
+//const apresentacao = "meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidadeDeNascimento;
+
+//template string 
+//acento grave: ``
+const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`;
+
+console.log(apresentacao)
